64fb51a6d2ae116e5e5d595f17113038311c9fcd
QWERT <microsoftman535@gmail.com>
2018-02-08 19:21:40 +0200

    save

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+function showPage() {
+    document.getElementById("loaderSection").style.display = "none";
+    document.getElementById("headerReg").style.display = "block";
+    document.getElementById("mainReg").style.display = "block";
+}
